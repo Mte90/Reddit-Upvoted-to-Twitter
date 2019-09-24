09b3c14c4319bad5dd6ede70d76c74ebb94e13c8
Daniele Scasciafratte <mte90net@gmail.com>
2019-09-24 19:24:11 +0200

    updates

--- bot.py
+++ bot.py
@@ -1,14 +1,22 @@
 #! /usr/bin/python3
 import praw
+import os
 
 reddit = praw.Reddit('mte90')
-
-print(reddit.user.me())
 
 print("Logged in as: " + str(reddit.user.me()))
 print()
 
 user = reddit.redditor('mte90')
+posts = {}
+
+if os.path.exists('./posts.json'):
+    f = open("./posts.json", "r")
+    posts = f.read()
 
 for post in user.upvoted(limit=100):
-    print(post.title + ' - ' + str(post.subreddit) + ' - ' + post.id + ' - ' + post.permalink)
+    print(post.id)
+    posts[post.id].append({'title': post.title, 'subreddit': str(post.subreddit).lower()})
+#    print(post.title + ' - ' + str(post.subreddit) + ' - ' + post.id + ' - ' + post.permalink)
+
+print(posts)
